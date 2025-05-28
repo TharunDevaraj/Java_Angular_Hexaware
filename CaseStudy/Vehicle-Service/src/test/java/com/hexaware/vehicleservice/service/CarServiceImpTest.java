77d8cb747fd9cd837a4ad7eaef73645a31e81b1e
TharunDevaraj <tharundevaraj0603@gmail.com>
2025-05-28 08:48:49 +0530

    Added unit testing for Vehicle-Service

---
+++ CaseStudy/Vehicle-Service/src/test/java/com/hexaware/vehicleservice/service/CarServiceImpTest.java
@@ -0,0 +1,69 @@
+package com.hexaware.vehicleservice.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CarServiceImpTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testAddCar() {
+		
+	}
+
+	@Test
+	void testGetCarById() {
+		
+	}
+
+	@Test
+	void testGetAllCars() {
+		
+	}
+
+	@Test
+	void testUpdateCar() {
+		
+	}
+
+	@Test
+	void testDeleteCarById() {
+		
+	}
+
+	@Test
+	void testGetAvailableCars() {
+		
+	}
+
+	@Test
+	void testMarkCarAsUnavailable() {
+		
+	}
+
+	@Test
+	void testMarkCarAsAvailable() {
+		
+	}
+
+	@Test
+	void testSearchVehicles() {
+		
+	}
+
+	@Test
+	void testIsCarAvailable() {
+		
+	}
+
+}
