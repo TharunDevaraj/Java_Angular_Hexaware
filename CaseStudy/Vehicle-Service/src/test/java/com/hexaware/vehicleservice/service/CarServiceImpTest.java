55d89d7e9ea2bab83697a6ca27d2dcbcd35196fb
TharunDevaraj <tharundevaraj0603@gmail.com>
2025-06-01 21:26:13 +0530

    Implemented testing methods for Vehicle-service

--- CaseStudy/Vehicle-Service/src/test/java/com/hexaware/vehicleservice/service/CarServiceImpTest.java
+++ CaseStudy/Vehicle-Service/src/test/java/com/hexaware/vehicleservice/service/CarServiceImpTest.java
@@ -1,13 +1,35 @@
 package com.hexaware.vehicleservice.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
+import com.hexaware.vehicleservice.dto.CarDTO;
+import com.hexaware.vehicleservice.entity.Car;
+import com.hexaware.vehicleservice.exception.CarNotFoundException;
+
+@SpringBootTest
 class CarServiceImpTest {
 
+	@Autowired
+	CarServiceImp carService;
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -17,53 +39,117 @@ class CarServiceImpTest {
 	}
 
 	@Test
+	@Disabled
 	void testAddCar() {
-		
+		CarDTO sampleCarDTO = new CarDTO();
+        sampleCarDTO.setCarName("Toyota Corolla");
+        sampleCarDTO.setYear(2020);
+        sampleCarDTO.setMake("Toyota");
+        sampleCarDTO.setCarStatus("available");
+        sampleCarDTO.setLocation("New York");
+        sampleCarDTO.setPassengerCapacity(5);
+        sampleCarDTO.setPricePerDay(49.99);
+        
+        Car result = carService.addCar(sampleCarDTO);
+        assertNotNull(result);
+        assertEquals("Toyota Corolla", result.getCarName());
 	}
 
 	@Test
-	void testGetCarById() {
+	@Disabled
+	void testGetCarById() throws CarNotFoundException {
+		CarDTO carDTO = carService.getCarById(202L);
+        assertEquals("Toyota Corolla", carDTO.getCarName());
 		
+        assertThrows(CarNotFoundException.class, () -> carService.getCarById(10L));
 	}
 
 	@Test
+	@Disabled
 	void testGetAllCars() {
 		
+		 List<Car> cars = carService.getAllCars();
+	        assertEquals(6, cars.size());
 	}
 
 	@Test
-	void testUpdateCar() {
+	@Disabled
+	void testUpdateCar() throws CarNotFoundException {
+		
+		CarDTO sampleCarDTO=new CarDTO();
+		sampleCarDTO.setLocation("Hyderabad");
+		
+		Car updatedCar = carService.updateCar(202L, sampleCarDTO);
+        assertEquals("Hyderabad", updatedCar.getLocation());
+        
+        assertThrows(CarNotFoundException.class, () -> carService.updateCar(99L, sampleCarDTO));
+	}
+
+	@Test
+	@Disabled
+	void testDeleteCarById() throws CarNotFoundException {
+		
+		carService.deleteCarById(302L);
+		
+		assertThrows(CarNotFoundException.class, () -> carService.getCarById(302L));
 		
 	}
 
 	@Test
-	void testDeleteCarById() {
-		
-	}
-
-	@Test
+	@Disabled
 	void testGetAvailableCars() {
 		
-	}
-
-	@Test
-	void testMarkCarAsUnavailable() {
+		List<Car> availableCars = carService.getAvailableCars();
+        assertEquals(4, availableCars.size());
 		
 	}
 
 	@Test
-	void testMarkCarAsAvailable() {
+	@Disabled
+	void testFindAvailableCarsByFilter() {
+		
+		List<Car> result = carService.findAvailableCarsByFilter("New York", 5, LocalDate.now(), LocalDate.now().plusDays(2));
+        assertEquals(1, result.size());
 		
 	}
 
 	@Test
+	@Disabled
 	void testSearchVehicles() {
 		
+		List<Car> result = carService.searchVehicles("New York", 5);
+        assertEquals(1, result.size());
+		
 	}
 
 	@Test
+	@Disabled
 	void testIsCarAvailable() {
 		
+		assertTrue(carService.isCarAvailable(352L));
+		
+		assertFalse(carService.isCarAvailable(152L));
+		
+	}
+
+	@Test
+	@Disabled
+	void testUpdateCarPricing() throws CarNotFoundException {
+		
+		Car updatedCar = carService.updateCarPricing(352L, 59.99);
+        assertEquals(59.99, updatedCar.getPricePerDay());
+        
+        assertThrows(CarNotFoundException.class, () -> carService.updateCarPricing(400L, 59.99));
+		
+	}
+
+	@Test
+	@Disabled
+	void testUpdateVehicleStatus() throws CarNotFoundException {
+		Car updated = carService.updateVehicleStatus(352L, "rented");
+        assertEquals("rented", updated.getCarStatus());
+        
+        assertThrows(CarNotFoundException.class, () -> carService.updateVehicleStatus(400L, "maintenance"));
 	}
 
 }
